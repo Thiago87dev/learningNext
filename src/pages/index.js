1ae1610c826c0a8f37ece28e7d83983850b3d8cc
thiago alves <thiago87gsa@gmail.com>
2023-12-25 11:17:43 -0300

    add time in home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title> Pagina Home </title>
-      <meta name="keywords" content="calça, camisa, boné"/>
-      <meta name="description" content="Encontre a melhor roupa para voce"/>
-    </Head>
+      <Head>
+        <title> Pagina Home </title>
+        <meta name="keywords" content="calça, camisa, boné" />
+        <meta name="description" content="Encontre a melhor roupa para voce" />
+      </Head>
       <div className={styles.container}>
         <h1 className={styles.title}>Home</h1>
-        <Image src="/images/pinguin.png" width={300} height={300} alt="Pinguins"/>
+        <Image
+          src="/images/pinguin.png"
+          width={300}
+          height={300}
+          alt="Pinguins"
+        />
         <ul>
           <li>
             <Link href="/products">Produtos</Link>
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,15 +1,70 @@
 import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function Products(){
-    return (
-        <>
-          <h1>Products</h1>
-          <ul>
-            <li><Link href="/products/shirt">Camisetas</Link></li>
-            <li><Link href="/products/pants">Calças</Link></li>
-            <li><Link href="/">Voltar</Link></li>
-            <li></li>
-          </ul>
-        </>
-      );
+export default function Products() {
+  const [bgColor, setBgcolor] = useState();
+  const [caminhoImg, setCaminhoImg] = useState("/images/manha.png");
+  const [saudacao, setSaudacao] = useState();
+  const [horaAtual, setHoraAtual] = useState();
+  const [minutoAtual, setminutoAtual] = useState();
+  const [segundosAtual, setSegundosAtual] = useState();
+
+  useEffect(() => {
+    function updateTime() {
+      const currentHour = new Date().getHours();
+      const currentMinutes = new Date().getMinutes();
+      const currentseconds = new Date().getSeconds();
+      setHoraAtual(currentHour);
+      setminutoAtual(currentMinutes);
+      if(currentseconds < 10){
+        setSegundosAtual(`0${currentseconds}`);
+      } else{
+        setSegundosAtual(currentseconds )
+      }
+
+      if (currentHour >= 6 && currentHour < 12) {
+        setBgcolor("blue");
+        setCaminhoImg("/images/manha.png");
+        setSaudacao("Bom dia");
+      } else if (currentHour >= 12 && currentHour < 18) {
+        setBgcolor("green");
+        setCaminhoImg("/images/tarde.png");
+        setSaudacao("boa tarde");
+      } else {
+        setBgcolor("#ffcbdb");
+        setCaminhoImg("/images/noite.png");
+        setSaudacao("boa noite");
+      }
+    }
+
+    updateTime();
+
+    const intervalId = setInterval(updateTime, 1000)
+
+    return () => clearInterval(intervalId)
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: bgColor }}>
+      <Image src={caminhoImg} width={300} height={300} alt="manha" />
+      <h1>Products</h1>
+      <h2>
+        Agora são {horaAtual}:{minutoAtual}:{segundosAtual}
+      </h2>
+      <h2>{saudacao}</h2>
+      <ul>
+        <li>
+          <Link href="/products/shirt">Camisetas</Link>
+        </li>
+        <li>
+          <Link href="/products/pants">Calças</Link>
+        </li>
+        <li>
+          <Link href="/">Voltar</Link>
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
